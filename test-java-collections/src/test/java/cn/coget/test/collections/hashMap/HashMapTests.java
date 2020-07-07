fbaf255ff0bbb1a0da3bf46fe0aa3f98f32f4cfa
cherishsince <cherishsince@aliyun.com>
2020-07-07 22:32:17 +0800

    repeatKey tests.

--- test-java-collections/src/test/java/cn/coget/test/collections/hashMap/HashMapTests.java
+++ test-java-collections/src/test/java/cn/coget/test/collections/hashMap/HashMapTests.java
@@ -25,8 +25,20 @@ public class HashMapTests {
     @Test
     public void putItemTest() {
         hashMap.put("key1", 10);
+        Assert.assertTrue("hash map size request 1!", hashMap.size() == 1);
+    }
+
+    @Test
+    public void repeatKeyTest() {
         hashMap.put("key1", 10);
-        hashMap.put("key2", 10);
+        hashMap.put("key1", 10);
+        Assert.assertTrue("hash map size request 1!", hashMap.size() == 1);
+    }
+
+    @Test
+    public void nullKeyTest() {
+        hashMap.put("key1", 10);
+        hashMap.put(null, 10);
         Assert.assertTrue("hash map size request 1!", hashMap.size() == 1);
     }
 }
