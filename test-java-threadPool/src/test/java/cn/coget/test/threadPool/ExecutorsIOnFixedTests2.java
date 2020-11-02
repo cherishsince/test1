ded9857e574e1fbd7b0810adb33e7bb8229651e8
cherishsince <cherishsince@aliyun.com>
2020-11-02 19:27:05 +0800

    Condition 操作

---
+++ test-java-threadPool/src/test/java/cn/coget/test/threadPool/ExecutorsIOnFixedTests2.java
@@ -0,0 +1,25 @@
+package cn.coget.test.threadPool;
+
+import org.junit.Test;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * author: Sin
+ * time: 2020/7/13 16:09
+ */
+public class ExecutorsIOnFixedTests2 {
+
+    private static final ExecutorService threadPool = Executors.newFixedThreadPool(10);
+
+    @Test
+    public void setup() {
+
+    }
+
+    @Test
+    public void ExecutorFixedTest() {
+
+    }
+}
